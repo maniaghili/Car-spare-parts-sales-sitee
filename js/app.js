7ab74e1709c8d8478dc495f7fa2909bf4dd92df8
maniaghili <aligholimani@gmail.com>
2024-06-15 10:58:11 +0330

    UPdate moshaver

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ let $ = document
 let fastp = $.querySelector('.b-t')
 let fastps = $.querySelector('.s-t')
 let fastpss = $.querySelector('.d-t')
-
-
+let moshaver = $.querySelector('.bg_gradient_main_site')
+let moshaverr = $.querySelector('.moshaver')
 
 
 
@@ -12,13 +12,23 @@ let fastpss = $.querySelector('.d-t')
 fastps.addEventListener('click',fastpp)
 fastp.addEventListener('click',fastpp)
 fastpss.addEventListener('click',fastpp)
-
+moshaverr.addEventListener('click',moshaveri)
+moshaver.addEventListener('click',moshaveri)
 
 
 function fastpp(){
   Swal.fire({
     title: 'خرید',
     text: ' محصول به سبد خرید اضافه شد',
+    icon: 'success',
+    confirmButtonText: 'تایید'
+  })
+}
+
+function moshaveri(){
+  Swal.fire({
+    title: 'مشاوره',
+    text: 'درخواست شما برای مشاوره ثبت شد و پشتیبانان ما در اسرع وقت با شما تماس خواهد گرفت',
     icon: 'success',
     confirmButtonText: 'تایید'
   })
